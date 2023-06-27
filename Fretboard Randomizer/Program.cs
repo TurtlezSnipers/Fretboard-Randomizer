a4d636fdb5ace5c645b01f5227c8ea8c0bcd1b83
Diego G <73109487+TurtlezSnipers@users.noreply.github.com>
2023-06-27 16:26:46 -0700

    Add files via upload

---
+++ Fretboard Randomizer/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Visual_Studio_Code;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Title = ("Fretboard Randomizer");
+        string[] notes = {"A", "A#/Bb", "B", "C", "C#/Db", "D", "D#/Eb", "E", "F", "F#/Gb", "G", "G#/Ab"};
+        Console.ForegroundColor = ConsoleColor.White;
+        NewProblem:
+        Random numbergenerator = new Random();
+        int num01 = numbergenerator.Next(0,12);
+
+        Console.WriteLine(notes[num01]);
+        Console.ReadKey();
+        goto NewProblem;
+    }
+}
